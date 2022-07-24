8d3c101c3ea0646680dd06aae4ccfef30fed1d32
Niraj-Fonseka <fonseka.live@gmail.com>
2022-07-24 10:59:19 -0500

    stil working on this

--- key-server/go.mod
+++ key-server/go.mod
@@ -4,7 +4,9 @@ go 1.18
 
 require (
 	cloud.google.com/go/secretmanager v1.5.0
+	cloud.google.com/go/storage v1.22.1
 	github.com/gin-gonic/gin v1.8.1
+	google.golang.org/api v0.85.0
 	google.golang.org/genproto v0.0.0-20220617124728-180714bec0ad
 )
 
@@ -20,8 +22,10 @@ require (
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/google/uuid v1.1.2 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.1.0 // indirect
 	github.com/googleapis/gax-go/v2 v2.4.0 // indirect
+	github.com/googleapis/go-type-adapters v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
@@ -35,7 +39,7 @@ require (
 	golang.org/x/oauth2 v0.0.0-20220608161450-d0670ef3b1eb // indirect
 	golang.org/x/sys v0.0.0-20220615213510-4f61da869c0c // indirect
 	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/api v0.85.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220609144429-65e65417b02f // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/grpc v1.47.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
--- key-server/server.go
+++ key-server/server.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
+
+	storage "cloud.google.com/go/storage"
+	"google.golang.org/api/iterator"
 
 	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
 
@@ -13,12 +17,39 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", GetPubKey)
+	r.GET("/ping", GetPubKeySM)
 
 	r.Run()
 }
 
-func GetPubKey(c *gin.Context) {
+func GetPubKeySrorage(c *gin.Context) {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		// TODO: Handle error.
+		log.Fatal(err)
+	}
+
+	bkt := client.Bucket("oss-pubkeys")
+
+	query := &storage.Query{Prefix: ""}
+
+	var names []string
+	it := bkt.Objects(ctx, query)
+	for {
+		attrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		names = append(names, attrs.Name)
+	}
+	fmt.Println(names)
+}
+
+func GetPubKeySM(c *gin.Context) {
 	ctx := context.Background()
 	client, err := secretmanager.NewClient(ctx)
 	if err != nil {
@@ -29,7 +60,7 @@ func GetPubKey(c *gin.Context) {
 	defer client.Close()
 
 	req := &secretmanagerpb.CreateSecretRequest{
-		Parent: "keyname",
+		Parent: "test-project-1223",
 		Secret: &secretmanagerpb.Secret{Name: "keyname"},
 		// TODO: Fill request struct fields.
 		// See https://pkg.go.dev/google.golang.org/genproto/googleapis/cloud/secretmanager/v1#CreateSecretRequest.
@@ -39,8 +70,8 @@ func GetPubKey(c *gin.Context) {
 		// TODO: Handle error.
 		log.Fatal(err)
 	}
-	// TODO: Use resp.
-	_ = resp
+
+	fmt.Println(resp)
 	c.JSON(http.StatusOK, gin.H{
 		"message": "pong",
 	})
