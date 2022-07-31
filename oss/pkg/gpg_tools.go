4bf4f70403771a3048e9f8429bd45cda63f9b096
Niraj-Fonseka <fonseka.live@gmail.com>
2022-07-31 17:54:06 -0500

    directory cleanup

--- oss/pkg/gpg_tools.go
+++ oss/pkg/gpg_tools.go
@@ -15,34 +15,56 @@ import (
 )
 
 func GenerateKeyPair(username, email, comment string) []byte {
+	path := fmt.Sprintf("%s/.oss", os.Getenv("HOME"))
 	config := gpgeez.Config{Expiry: 365 * 24 * time.Hour}
 	key, err := gpgeez.CreateKey(username, comment, email, &config)
 	if err != nil {
 		fmt.Printf("Something went wrong: %v", err)
+		os.Exit(1)
 		return []byte{}
 	}
 	_, err = key.Armor()
 	if err != nil {
 		fmt.Printf("Something went wrong: %v", err)
+		os.Exit(1)
 		return []byte{}
 	}
 
 	_, err = key.ArmorPrivate(&config)
 	if err != nil {
 		fmt.Printf("Something went wrong: %v", err)
+		os.Exit(1)
 		return []byte{}
+	}
+
+	_, err = os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			err := os.MkdirAll(path, 0755)
+			if err != nil {
+				fmt.Printf("unable to create the required directory at path : %s", path)
+				os.Exit(1)
+			}
+		} else {
+			fmt.Printf("something went wrong when creating the required directory  : %v", err)
+			os.Exit(1)
+		}
 	}
 
 	pub := key.Keyring()
 	pvt := key.Secring(&config)
-	pub_err := ioutil.WriteFile("oss_pub.gpg", pub, 0666)
-	if err != nil {
+	pub_err := ioutil.WriteFile(path+"/oss_pub.gpg", pub, 0666)
+	if pub_err != nil {
 		log.Printf("error when writing public key : %v\n", pub_err)
+		os.Exit(1)
+
 		return []byte{}
 	}
-	pvt_err := ioutil.WriteFile("oss_pvt.gpg", pvt, 0666)
-	if err != nil {
-		log.Printf("error when writing public key : %v\n", pvt_err)
+	pvt_err := ioutil.WriteFile(path+"/oss_pvt.gpg", pvt, 0666)
+	if pvt_err != nil {
+		log.Printf("error when writing the private key : %v\n", pvt_err)
+		os.Exit(1)
+
 		return []byte{}
 	}
 
@@ -85,6 +107,7 @@ func Encrypt(data string, pubKey []byte) (string, error) {
 }
 
 func Decrypt(encryptedString string) (string, error) {
+	path := fmt.Sprintf("%s/.oss", os.Getenv("HOME"))
 
 	const passphrase = "" //go/crypto doesn't support passpharse yet
 
@@ -92,10 +115,20 @@ func Decrypt(encryptedString string) (string, error) {
 	var entity *openpgp.Entity
 	var entityList openpgp.EntityList
 
-	// Open the private key file
-	keyringFileBuffer, err := os.Open("/home/hungryotter/go/src/open-secret-share/oss/oss_pvt.gpg")
+	_, err := os.Stat(path)
 	if err != nil {
-		return "", err
+		fmt.Printf("uninitialized. please run the command -> oss init")
+		os.Exit(1)
+	}
+	if os.IsNotExist(err) {
+		fmt.Printf("uninitialized. please run the command -> oss init")
+		os.Exit(1)
+	}
+
+	// Open the private key file
+	keyringFileBuffer, err := os.Open(path + "/oss_pvt.gpg")
+	if err != nil {
+		log.Println("unable to find the private key : please re-initialize the app")
 	}
 	defer keyringFileBuffer.Close()
 	entityList, err = openpgp.ReadKeyRing(keyringFileBuffer)
